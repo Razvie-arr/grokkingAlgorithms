079f795f83fa20742addb499e6770bd038c3ad38
razviearr <vladbyastinov@mail.ru>
2021-06-14 01:34:17 +0900

    сортировка выбором

---
+++ 2_insertion_sort.py
@@ -0,0 +1,19 @@
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def insertion_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+print(insertion_sort([10, 213, 35, 6, 8, 1]))
